89d6f4e5fd703980d5c7ac72709c695008feb8e5
mattsedlar <sedlarm@gmail.com>
2015-10-22 13:26:21 -0700

    Additional analysis

--- scripts/raw/rmodel.R
+++ scripts/raw/rmodel.R
@@ -8,31 +8,50 @@ x4 <- mtcars$disp
 x5 <- mtcars$carb
 x6 <- mtcars$wt
 
-fit <- lm(y ~ as.factor(x1) + as.factor(x2) + x3 + x4 + as.factor(x5) + x6)
+fit.hp <- lm(y ~ x3)
+fit.disp <- lm(y ~ x4)
+fit.wt <- lm(y ~ x6)
 
-# Multiple R-squared 0.8777 means 88% of the variation in mpg can be explained by
-# variation in the independent X variables
+# Residual analysis for horsepower
+plot(x=x3,y=fit.hp$residuals)
+hist(x=fit.hp$residuals)
 
-# Residual analysis
+d.hp <- density(fit.hp$residuals)
+plot(d.hp)
 
-plot(x=predict(fit),y=fit$residuals)
-# transmission type
-plot(x=x1,y=fit$residuals)
-# cylinders
-plot(x=x2,y=fit$residuals)
-# horsepower
-plot(x=x3,y=fit$residuals)
-# displacement
-plot(x=x4,y=fit$residuals)
-# carburetors
-plot(x=x5,y=fit$residuals)
-# weight
-plot(x=x6,y=fit$residuals)
+# Residual analysis for weight
+plot(x=x6,y=fit.wt$residuals)
+hist(x=fit.wt$residuals)
+d.wt <- density(fit.wt$residuals)
+# skewed right
+plot(d.wt)
 
-# checking normality
-hist(x=fit$residuals)
+# Residual analysis for displacement
+plot(x=x4,y=fit.disp$residuals)
+hist(x=fit.disp$residuals)
+d.disp <- density(fit.disp$residuals)
+# skewed right
+plot(d.disp)
 
-# T TEST
-# critical value on df 20 with 95% sig. is 2.0860
+# test mean = median = mode on disp residuals
 
+mean.disp <- mean(fit.disp$residuals)
+median.disp <- median(fit.disp$residuals)
+table_data <- table(signif(fit.disp$residuals,1))
+samplemode <- subset(table_data, table_data==max(table_data))
+samplemode <- as.numeric(names(samplemode))
 
+# testing assumption of strong correlation between disp and wt, hp
+# disp v wt
+plot(x=x4,y=x6)
+cor(x4,x6)
+disp.wt <- lm(x4 ~ x6)
+plot(x=x4,y=disp.wt$residuals)
+hist(disp.wt$residuals)
+
+# disp v hp
+plot(x=x4,y=x3)
+cor(x4,x3)
+disp.hp <- lm(x4 ~ x3)
+plot(x=x4,y=disp.hp$residuals)
+hist(disp.hp$residuals)
